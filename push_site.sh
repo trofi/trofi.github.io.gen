fcadc0492b869f9c61d5bf65f29be429810761c3
Sergei Trofimovich <slyfox@gentoo.org>
2013-11-26 00:02:52 +0300

    push_site.sh: simple helper to publish stuff
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

---
+++ push_site.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+commit_id=`git log -1 --format=format:"%h: %s (%ai)"`
+
+cd _site
+if [ ! -d .git ]; then
+    git init
+    git remote add --fetch origin git@github.com:trofi/trofi.github.io.git
+fi
+git add .
+git commit -s -m "sync trofi.github.io.git:${commit_id}" .
+git push --force -u origin master
