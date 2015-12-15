50729be0fdc0edbbb9b08794e05b5c1b6aaf4f59
Sergei Trofimovich <siarheit@google.com>
2015-12-15 22:21:53 +0000

    run_watcher.sh: link site generator dynamically
    Shrinks binary size from 137MB to 137KB.
    That speeds rebuilds and startup time.
    
    Signed-off-by: Sergei Trofimovich <siarheit@google.com>

--- run_watcher.sh
+++ run_watcher.sh
@@ -5,8 +5,8 @@ site_exe=dist/build/trofi.github.io.gen/trofi.github.io.gen
 
 set -e
 
-ghc --make -O2 Setup.hs -o "${setup_exe}"
-"${setup_exe}" configure
+ghc --make -O2 Setup.hs -o "${setup_exe}" -dynamic
+"${setup_exe}" configure --enable-executable-dynamic
 "${setup_exe}" build
 
 "${site_exe}" clean
