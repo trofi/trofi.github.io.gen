94fc00c57c07d2e3a7f244c69bf819af2bd9c3a6
Sergei Trofimovich <slyich@gmail.com>
2022-06-11 09:49:28 +0100

    push_site.sh: make site pushes incremental

--- push_site.sh
+++ push_site.sh
@@ -8,7 +8,10 @@ cd _site
 if [ ! -d .git ]; then
     git init
     git remote add --fetch origin git@github.com:trofi/trofi.github.io.git
+    # Use pristine freshly generated _site state for new commit.
+    # Don't take anything from parent except for the commit ID itself.
+    git reset --soft origin/main
 fi
 git add .
 git commit -s -m "sync trofi.github.io.git:${commit_id}" .
-git push --force -u origin main
+git push -u origin main
