fbf3b3ee8bebd30f8000900f8e3a5116ad09fc52
Sergei Trofimovich <slyich@gmail.com>
2022-06-18 19:01:49 +0100

    run_watcher.sh: use --pure shell to make sure we don't miss depends

--- run_watcher.sh
+++ run_watcher.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env nix-shell
+#! nix-shell --pure
 #! nix-shell -p "haskellPackages.ghcWithPackages (p: [p.pandoc p.hakyll])"
 #! nix-shell -p graphviz
 #! nix-shell -p bash
 #! nix-shell -i bash
+
+export LANG=C.UTF-8
 
 setup_exe=./Setup
 site_exe=dist/build/trofi.github.io.gen/trofi.github.io.gen
