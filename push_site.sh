34cc86037786bceae651fe47acc6b1e0d4443d36
Sergei Trofimovich <slyich@gmail.com>
2022-07-31 13:02:49 +0100

    push_site.sh: use local repository as an object cache

--- push_site.sh
+++ push_site.sh
@@ -7,6 +7,16 @@ commit_id=`git log -1 --format=format:"%h: %s (%ai)"`
 cd _site
 if [ ! -d .git ]; then
     git init
+
+    # reuse local checkout's objects if possible
+    alternate_store=$(pwd)/../../trofi.github.io/.git/objects
+    if [ ! -d ${alternate_store} ]; then
+        echo "WARNING: '$alternate_store' checkout does not exist"
+        echo "WARNING: push will always refetch initial state."
+    else
+        echo "$alternate_store" >> .git/objects/info/alternates
+    fi
+
     git remote add --fetch origin git@github.com:trofi/trofi.github.io.git
     # Use pristine freshly generated _site state for new commit.
     # Don't take anything from parent except for the commit ID itself.
