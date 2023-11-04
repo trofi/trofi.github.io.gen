8be1590fc15c874b1738b207da91a406c50b718b
Sergei Trofimovich <slyich@gmail.com>
2023-11-04 20:17:10 +0000

    run_watcher.sh: enable threaded RTS and use all the known cores

--- run_watcher.sh
+++ run_watcher.sh
@@ -13,8 +13,8 @@ site_exe=dist/build/trofi.github.io.gen/trofi.github.io.gen
 set -e
 
 ghc --make -O2 Setup.hs -o "${setup_exe}" -dynamic -fforce-recomp
-"${setup_exe}" configure --enable-executable-dynamic
+"${setup_exe}" configure --enable-executable-dynamic --ghc-options=-threaded
 "${setup_exe}" build
 
 "${site_exe}" clean
-"${site_exe}" watch "$@"
+"${site_exe}" watch "$@" +RTS -N$(nproc)
